095657fdaf4add871483edff1d6800e87a79df45
gabrielbrunoot <130561602+gabrielbrunoot@users.noreply.github.com>
2024-03-27 08:01:06 -0300

    jogos.py

---
+++ jogos (1).py
@@ -0,0 +1,22 @@
+    import forca
+    import adivinhaçao
+    
+    def escolhe_jogo():
+        print("**********************************")
+        print("*******Escolha o seu jogo !*******")
+        print("**********************************")
+        
+        print("(1) forca (2) Adivinhaçao")
+        
+        jogo = int(input("Qual jogo"))
+        
+        if(jogo == 1):
+            print("jogando forca")
+            forca.jogar()
+        elif(jogo == 2):
+            print("jogando adivinhçao")
+            adivinhaçao.jogar()
+            
+    if(__name__ == "__main__"):
+        escolhe_jogo()
+         
