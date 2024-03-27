d80ae3837e3ebcb103cbf84cfada519e04b1f8e6
gabrielbrunoot <130561602+gabrielbrunoot@users.noreply.github.com>
2024-03-27 07:42:59 -0300

    forca.py

--- forca.py
+++ forca.py
@@ -3,15 +3,15 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
     
-    palevra_secreta = "maça".upper()
+    palavra_secreta = "Gabriel".upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
     letras_acertadas2 = []
     
-    for letra in (palevra_secreta):
+    for letra in (palavra_secreta):
         letras_acertadas2.append("_")
         
-        enforcou = false
-        acertou = false
+        enforcou = False
+        acertou = False
         arros = 0
         
         print(letras_acertadas)
@@ -21,11 +21,11 @@ def jogar():
             chute = input("qual letra?")
             chute = chute.strip().upper()
             
-            if (chute in palvra_secreta):
+            if (chute in palavra_secreta):
                 index = 0
-                for letra in palvra_secreta:
+                for letra in palavra_secreta:
                     if (chute == letra):
-                        letras_acertadas[index] = letras
+                        letras_acertadas[index] = letra
                         index += 1 
             else:
                 erros += 1 
@@ -40,6 +40,6 @@ def jogar():
                     print("Voce perdeu!!")
                     print("Fim de jogo")
                     
-                    if (__name__ == "__main__"):
-                        jogar()
+if (__name__ == "__main__"):
+    jogar()
             
