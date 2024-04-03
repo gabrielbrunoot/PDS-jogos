56ff4731bddf9a6efb050d09270df26e130294b6
gabrielbrunoot <130561602+gabrielbrunoot@users.noreply.github.com>
2024-04-02 21:24:56 -0300

    Update adivinhacao.py(1)

--- adivinhacao.py
+++ adivinhacao.py
@@ -28,3 +28,24 @@ def jogar():
                 print("tente novamente, voce tem: ", tentativas, "tentativas")
             else:
                 print("game over")
+
+def dica (numeroEscolhido: int):
+    print("Escolhido", numeroEscolhido, "Secreto", numeroSecreto)
+    if numeroEscolhido > numeroSecreto:
+        return "O numero escolhido é Maior que o numero secreto, ", numeroEscolhido
+    else:
+        return "O numero escolhido é Menor que o numero secreto", numeroEscolhido
+
+def NivelDificuldade():
+    print("Qual nivel de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Dificil")
+    dificuldade = int(input("Defina o nivel: "))
+    global tentativas
+    if (dificuldade == 1):
+        tentativas = 1
+    elif (dificuldade == 2):
+        tentativas = 8
+    else:
+        tentativas = 6
+
+54 jogar()
